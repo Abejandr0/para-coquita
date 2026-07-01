78865df58145726ec7bc2605fd3aa0ddbbcaefa2
Abejandro <abejandro@example.com>
2026-07-01 12:30:54 -0500

    Agregado modo Te Extrano y configuracion de produccion

--- coquita_project/settings.py
+++ coquita_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%$uz84)rw^j1ba65dh2h7q@l(vj1qz^d9=4c*p-5en&((lyy#2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,3 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Simplified static file serving.
+# https://whitenoise.evans.io/en/stable/django.html#add-compression-and-caching-support
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
